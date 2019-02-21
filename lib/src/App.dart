fbd8ce6efeddd40894d34cdf81c257d51c7ace7d
AYM1607 <u.g.a.mariano@gmail.com>
2019-02-20 21:39:23 -0600

    Removed the flutter example code

---
+++ lib/src/App.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Do more',
+      home: Text('Start'),
+    );
+  }
+}
