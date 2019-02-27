cadde064715e4e95c9b5e63dd2cd1a314ceb166c
AYM1607 <u.g.a.mariano@gmail.com>
2019-02-26 20:49:53 -0600

    Created [signIn] methos

--- lib/src/App.dart
+++ lib/src/App.dart
@@ -1,26 +1,6 @@
 import 'package:flutter/material.dart';
 
-import './models/event_model.dart';
-import './models/task_model.dart';
-import './models/user_model.dart';
-import './resources/firestore_provider.dart';
-
 class App extends StatelessWidget {
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Do more',
-      //home: Text('Start'),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('DO>'),
-        ),
-        body: Text('Tasks'),
-      ),
-    );
-  }
-}
-
-/* class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Do more',
