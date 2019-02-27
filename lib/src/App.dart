f7093ea73c8e1f509366c0ad749833834ac2df37
AYM1607 <u.g.a.mariano@gmail.com>
2019-02-26 20:59:09 -0600

    Added login button

--- lib/src/App.dart
+++ lib/src/App.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import './models/event_model.dart';
+import './models/summary_model.dart';
+import './models/task_model.dart';
+import './models/user_model.dart';
+import './resources/google_login_provider.dart';
 
 class App extends StatelessWidget {
   Widget build(BuildContext context) {
