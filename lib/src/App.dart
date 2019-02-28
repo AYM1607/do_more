e3d6c8423f1119b492a734f7c6e6de789992c93a
AYM1607 <u.g.a.mariano@gmail.com>
2019-02-27 18:34:24 -0600

    Test for the firebase storage provider

--- lib/src/App.dart
+++ lib/src/App.dart
@@ -1,12 +1,20 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import './models/event_model.dart';
-import './models/summary_model.dart';
-import './models/task_model.dart';
-import './models/user_model.dart';
 import './resources/google_sign_in_provider.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import './resources/firebase_storage_provider.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  AppState createState() => AppState();
+}
+
+class AppState extends State<App> {
+  File image;
+  FirebaseStorageProvider provider = FirebaseStorageProvider();
+  StorageUploadTask task;
+
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Do more',
