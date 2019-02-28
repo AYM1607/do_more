167472154ecc1ec30e861e8e14237fbdeefb39f3
AYM1607 <u.g.a.mariano@gmail.com>
2019-02-27 19:09:49 -0600

    Implemented the [deleteFile] method

--- lib/src/App.dart
+++ lib/src/App.dart
@@ -1,20 +1,6 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import './resources/google_sign_in_provider.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import './resources/firebase_storage_provider.dart';
 
-class App extends StatefulWidget {
-  AppState createState() => AppState();
-}
-
-class AppState extends State<App> {
-  File image;
-  FirebaseStorageProvider provider = FirebaseStorageProvider();
-  StorageUploadTask task;
-
+class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Do more',
