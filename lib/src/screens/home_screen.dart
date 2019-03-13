ae0f8515aa6a0a84ca6124506ca538b523b32fb4
AYM1607 <u.g.a.mariano@gmail.com>
2019-03-12 21:23:35 -0600

    Created and added extra animation to replace material progress indicator

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,18 +1,37 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import '../models/task_model.dart';
 import '../resources/authService.dart';
+import '../resources/firestore_provider.dart';
 import '../widgets/task_list_tile.dart';
+import '../widgets/loading_indicator.dart';
 
 class HomeScreen extends StatelessWidget {
   final _auth = authService;
+  final _firestore = firestoreProvider;
 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Main Screen'),
       ),
-      body: TaskListTile(task: TaskModel.sample()),
+      body: StreamBuilder(
+        stream: _firestore.getUserTasks('mariano159357'),
+        builder: (BuildContext context, AsyncSnapshot<List<TaskModel>> snap) {
+          if (!snap.hasData) {
+            return Center(
+              child: LoadingIndicator(),
+            );
+          }
+          return ListView(
+            padding: EdgeInsets.all(0.0),
+            children:
+                snap.data.map((task) => TaskListTile(task: task)).toList(),
+          );
+        },
+      ),
     );
   }
 }
---
+++ lib/src/widgets/loading_indicator.dart
@@ -0,0 +1,16 @@
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/material.dart';
+
+class LoadingIndicator extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Container(
+      width: 70,
+      height: 70,
+      child: FlareActor(
+        'assets/animations/loading_animation_looped.flr',
+        animation: 'Flip',
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+}
