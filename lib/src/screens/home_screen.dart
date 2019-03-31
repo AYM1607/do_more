4f450ade30ea44b0d781f4b7a7492d04cc0a2e78
AYM1607 <u.g.a.mariano@gmail.com>
2019-03-31 00:35:34 -0600

    Created new task screen

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
---
+++ lib/src/screens/new-task-screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewTaskScreen extends StatefulWidget {
+  @override
+  State<NewTaskScreen> createState() => _NewTaskScreenState();
+}
+
+class _NewTaskScreenState extends State<NewTaskScreen> {
+  Widget build(BuildContext context) {}
+}
