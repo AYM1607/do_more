4db0ab4e5382d6c5116479406fe0ce4951b55992
AYM1607 <u.g.a.mariano@gmail.com>
2019-03-31 23:42:27 -0600

    Added methods to allow the new task screen to update the blocs properties

--- lib/src/blocs/new_task_bloc.dart
+++ lib/src/blocs/new_task_bloc.dart
@@ -6,4 +6,20 @@ import '../models/task_model.dart';
 import '../resources/authService.dart';
 import '../resources/firestore_provider.dart';
 
-class NewTaskBloc {}
+class NewTaskBloc {
+  final AuthService _auth = authService;
+  FirebaseUser currentUser;
+
+  String text;
+  TaskPriority priority;
+  String event;
+
+  NewTaskBloc() {
+    setCurrentUser();
+  }
+
+  Future<void> setCurrentUser() async {
+    final user = await _auth.currentUser;
+    currentUser = user;
+  }
+}
