6865399151fa68c580c5f7cd335127bd894b2c08
AYM1607 <u.g.a.mariano@gmail.com>
2019-04-07 18:06:21 -0500

    Added firebase function to generate thumnails from uploaded images

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,50 @@
 import * as functions from 'firebase-functions';
+import { Storage } from '@google-cloud/storage';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const gcs = new Storage();
+
+import { tmpdir } from 'os';
+import { join, dirname } from 'path';
+
+import * as sharp from 'sharp';
+import * as fs from 'fs-extra';
+
+export const generateThumb = functions.storage.object().onFinalize(async object => {
+    // Find the bucket where the uploaded dile resides.
+    const bucket = gcs.bucket(object.bucket);
+    const filePathGcs = object.name;
+    const fileName = filePathGcs!.split('/').pop();
+
+    const bucketDirectory = dirname(filePathGcs!);
+    const workingDirectory = join(tmpdir(), 'thumbnails');
+    const tmpFilePath = join(workingDirectory, 'source.png');
+
+    if (fileName!.includes('thumb@') || !object.contentType!.includes('image')) {
+        console.log('Exiting function');
+        return false;
+    }
+
+    // Ensure that our directory exists.
+    await fs.ensureDir(workingDirectory);
+
+    // Download the source file to the temporary directory.
+    await bucket.file(filePathGcs!).download({
+        destination: tmpFilePath,
+    });
+
+    // Create the thumnail in 124 size.
+    const thumbnailName = `thumb@${fileName}`;
+    const thumbnailLocalPath = join(workingDirectory, thumbnailName);
+
+    await sharp(tmpFilePath)
+        .resize(124, 124)
+        .toFile(thumbnailLocalPath);
+
+    // Upload the resulting thumnail to the same directory as the original file.
+    await bucket.upload(thumbnailLocalPath, {
+        destination: join(bucketDirectory, thumbnailName),
+    });
+
+    // Exit the function deleting all the temprorary files.
+    return fs.remove(workingDirectory);
+});
