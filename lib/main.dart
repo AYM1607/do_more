21cc8697b02d41f55c9b7708a1ddecddb21ee777
AYM1607 <u.g.a.mariano@gmail.com>
2019-04-06 23:27:59 -0600

    Set the orientation of the whole app to only be portrait

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:flutter/services.dart';
 
 import 'src/App.dart';
 
-main() {
+main() async {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(App());
 }
