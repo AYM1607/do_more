118bb2671311cccf7de2ad02e54f054cd07f80e9
AYM1607 <u.g.a.mariano@gmail.com>
2019-04-24 15:48:33 -0500

    Made the populated drawer in the events screen use the data from the firebase user object

--- lib/src/screens/events_screen.dart
+++ lib/src/screens/events_screen.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart' hide AppBar;
 
+import '../blocs/events_bloc.dart';
 import '../widgets/app_bar.dart';
 import '../widgets/populated_drawer.dart';
 
-class EventsScreen extends StatelessWidget {
+class EventsScreen extends StatefulWidget {
+  @override
+  _EventsScreenState createState() => _EventsScreenState();
+}
+
+class _EventsScreenState extends State<EventsScreen> {
+  /// An instance of the bloc for this screen.
+  final EventsBloc bloc = EventsBloc();
+
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: PopulatedDrawer(
-        selectedScreen: Screen.events,
-      ),
-      appBar: AppBar(
-        title: 'My Events',
-        hasDrawer: true,
-      ),
+    return StreamBuilder(
+      stream: bloc.userStream,
+      builder: (context, AsyncSnapshot<FirebaseUser> userSnap) {
+        String userAvatarUrl, userDisplayName = '', userEmail = '';
+
+        if (userSnap.hasData) {
+          userAvatarUrl = userSnap.data.photoUrl;
+          userDisplayName = userSnap.data.displayName;
+          userEmail = userSnap.data.email;
+        }
+        return Scaffold(
+          drawer: PopulatedDrawer(
+            userAvatarUrl: userAvatarUrl,
+            userDisplayName: userDisplayName,
+            userEmail: userEmail,
+            selectedScreen: Screen.events,
+          ),
+          appBar: AppBar(
+            title: 'My Events',
+            hasDrawer: true,
+          ),
+        );
+      },
     );
   }
 }
